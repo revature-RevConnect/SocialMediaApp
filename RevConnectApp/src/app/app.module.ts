16ce770f49fcc7b1511bb49663380070a818bf08
Muhammad Amin <amindaimirdadi@gmail.com>
2022-05-11 17:46:48 -0400

    Creating like-button component & experimental like-button

--- RevConnectApp/src/app/app.module.ts
+++ RevConnectApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LikeButtonComponent } from './like-button/like-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ RevConnectApp/src/app/like-button/like-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-like-button',
+  templateUrl: './like-button.component.html',
+  styleUrls: ['./like-button.component.css']
+})
+export class LikeButtonComponent implements OnInit {
+  likeCount= 100;
+  isLiked = false;
+  
+  likeTheButton = () => {
+    if(this.isLiked)
+    this.likeCount--;
+    else
+    this.likeCount++;
+
+    this.isLiked = !this.isLiked
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
