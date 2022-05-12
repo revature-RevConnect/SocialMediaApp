50be91634a4acfef3ada1802e308c11e750d9cb7
Phirith Pheak <phirith@protonmail.com>
2022-05-12 09:59:35 -0500

    rough draft profile

--- RevConnectApp/src/app/app-routing.module.ts
+++ RevConnectApp/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ChatroomComponent } from "./pages/chatroom/chatroom.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { SettingsComponent } from "./pages/settings/settings.component";
 import { UserProfileComponent } from "./pages/user-profile/user-profile.component";
+import { AuthGuard } from "@auth0/auth0-angular";
 
 const routes: Routes = [
     {path:'', component:HomeComponent},
-    {path:'chatroom', component:ChatroomComponent},
-    {path:'settings', component:SettingsComponent},
-    {path:'user-profile', component:UserProfileComponent}
+    {path:'chatroom', component:ChatroomComponent, canActivate: [AuthGuard]},
+    {path:'settings', component:SettingsComponent, canActivate: [AuthGuard]},
+    {path:'user-profile', component:UserProfileComponent, canActivate: [AuthGuard]}
 
 ];
 
--- RevConnectApp/src/app/pages/user-profile/user-profile.component.ts
+++ RevConnectApp/src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  profileJson!: string; 
 
-  constructor() { }
-
+  constructor(public auth: AuthService) {}
+    
   ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile,null,2))
+
+    )
+
   }
 
 }
