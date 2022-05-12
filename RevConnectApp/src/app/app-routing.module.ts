8db08146c4a10e625510bddc566c7c681295209e
Kevin Lee <klee5760@hotmail.com>
2022-05-12 13:29:12 -0500

    05-12-2022 Updated

---
+++ RevConnectApp/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ChatroomComponent } from "./pages/chatroom/chatroom.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { SettingsComponent } from "./pages/settings/settings.component";
+import { UserProfileComponent } from "./pages/user-profile/user-profile.component";
+
+const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'chatroom', component:ChatroomComponent},
+    {path:'settings', component:SettingsComponent},
+    {path:'user-profile', component:UserProfileComponent}
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+
+
--- RevConnectApp/src/app/app.module.ts
+++ RevConnectApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DarkModeComponent } from './dark-mode/dark-mode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkModeComponent
   ],
   imports: [
     BrowserModule
