f0a5964409b3cc3bad7d9d8ea97055a10a99cbc0
Tryggve Rogness <tcrogness@gmail.com>
2022-05-24 12:04:48 -0500

    fixed urls

--- MockarooData/MOCK_CHECKOUT.sql
+++ MockarooData/MOCK_CHECKOUT.sql
@@ -1,4 +1,14 @@
-SELECT * FROM RevConnect.Users;
-SELECT * FROM RevConnect.Posts;
-SELECT * FROM RevConnect.Comments;
-SELECT * FROM RevConnect.Likes;
+--Highlight the commands you want to run and hit run. 
+--If you run the file without doing that, you will take apart the tables and schema.
+
+SELECT * FROM RevSocial.Users;
+SELECT * FROM RevSocial.Posts;
+SELECT * FROM RevSocial.Comments;
+SELECT * FROM RevSocial.Likes;
+
+DROP TABLE RevSocial.Likes;
+DROP TABLE RevSocial.Comments;
+DROP TABLE RevSocial.Posts;
+DROP TABLE RevSocial.Users;
+
+DROP SCHEMA [ IF EXISTS ] RevSocial;
